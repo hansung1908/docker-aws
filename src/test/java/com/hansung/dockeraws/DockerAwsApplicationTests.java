1f0f9c3387d8f6bfae2601c92dbad5c146652bb5
hansung1908 <hansung1908@naver.com>
2023-08-24 15:08:11 +0900

    테스트용 프로젝트

---
+++ src/test/java/com/hansung/dockeraws/DockerAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansung.dockeraws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
